9759980bdc8bf591afb8d9221b5413fc8ff6d655
beefstew2002 <stuarteliason@hotmail.com>
2026-05-18 11:26:13 -0600

    This should have been added already

---
+++ src/dcc/houdini/publish/asset_publish_dialog.py
@@ -0,0 +1,8 @@
+from Qt import QtWidgets
+
+
+class AssetPublishDialog(QtWidgets.QDialog):
+    def __init__(self):
+        # what more do you want
+        # I just need to commit the changes
+        pass
